5fc5a23bc2fd9e136f2ab4737d0ad37267e3f20a
Elicarlos <elicarlosantos_@hotmail.com>
2021-01-15 17:58:37 -0300

    urur

--- library/catolog/admin.py
+++ library/catolog/admin.py
@@ -7,12 +7,22 @@ class BookAdmin(admin.ModelAdmin):
 
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death   ')]
 
 class GenreAdmin(admin.ModelAdmin):
     pass
 
 class BookInstanceAdmin(admin.ModelAdmin):
     list_filter = ('status', 'due_back')
+
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    ) 
 
 class LanguageAdmin(admin.ModelAdmin):
     pass
